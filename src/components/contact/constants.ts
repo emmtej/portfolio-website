99ce3aa9b58f19fd8ae8aac867ed3283949c20c5
Emmanuel T. <contact@emmtej.com>
2026-04-01 16:40:21 +0200

    Add constants for social links in contact component

---
+++ src/components/contact/constants.ts
@@ -0,0 +1,20 @@
+export const SOCIALS = [
+  {
+    label: "GitHub",
+    href: "https://github.com/emmtej",
+    value: "emmtej",
+    icon: "GH",
+  },
+  {
+    label: "LinkedIn",
+    href: "https://linkedin.com",
+    value: "emmanuel-tejeda",
+    icon: "LI",
+  },
+  {
+    label: "Email",
+    href: "mailto:contact@emmanueltejeda.com",
+    value: "contact",
+    icon: "EM",
+  },
+];
